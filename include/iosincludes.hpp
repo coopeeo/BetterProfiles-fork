649e1195342529315e33d8836e8cc7b9c23c0f75
Cooper <coop@coopeeo.xyz>
2024-05-19 12:20:52 -0500

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- include/iosincludes.hpp
+++ include/iosincludes.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#ifdef GEODE_IS_IOS
+#if (GEODE_TARGET_PLATFORM == "iOS")
 #pragma message("iOS Includes: The current platform is iOS, so there will be std included and other files so iOS works.")
 #include <string>
 #include <optional>
