a4a414cc5217702aa0526c56a27226e45fee571b
Cooper <coop@coopeeo.xyz>
2024-05-19 11:54:40 -0500

    try this stuff

--- include/iosincludes.hpp
+++ include/iosincludes.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#if (${GEODE_TARGET_PLATFORM} STREQUAL "iOS" OR IOS)
+#if GEODE_TARGET_PLATFORM STREQUAL "iOS" OR IOS
 #pragma message("iOS Includes: The current platform is iOS, so there will be std included and other files so iOS works.")
 #include <string>
 #include <optional>
