7381bbaa28a3259155a0fbc4f1f353f20f2aeda3
Cooper <coop@coopeeo.xyz>
2024-05-19 12:12:28 -0500

    ugh just work

--- include/iosincludes.hpp
+++ include/iosincludes.hpp
@@ -1,12 +1,12 @@
 #pragma once
-#ifdef GEODE_IS_IOS
-#pragma message(STATUS "iOS Includes: The current platform is iOS, so there will be std included and other files so iOS works.")
+#if CMAKE_SYSTEM_NAME == "iOS"
+#pragma message("iOS Includes: The current platform is iOS, so there will be std included and other files so iOS works.")
 #include <string>
 #include <optional>
 #include <vector>
 #include <Geode/Geode.hpp>
 #include <Geode/cocos/include/ccTypes.h>
 #else
-#pragma message(STATUS "iOS Includes: not iOS, so nothing will be included")
+#pragma message("iOS Includes: not iOS, so nothing will be included")
 #endif
 // ? This is here for importing classes on iOS because iOS doesn't like when string isn't imported!
